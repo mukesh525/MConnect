bc856c76d60c4c866640b5a00967906bf23b80f7
Mukesh Jha <mukeshjha2007@gmail.com>
2016-03-14 11:47:59 +0530

    14 st March

--- app/src/main/java/in/vmc/mcubeconnect/databse/MDatabase.java
+++ app/src/main/java/in/vmc/mcubeconnect/databse/MDatabase.java
@@ -17,17 +17,21 @@ import in.vmc.mcubeconnect.model.VisitData;
 public class MDatabase {
     private SiteHelper mHelper;
     private SQLiteDatabase mDatabase;
+    public static final int ALL = 0;
+    public static final int LIKE = 1;
+    public static final int OFFER = 2;
+    public static final int VISIT = 3;
 
     public MDatabase(Context context) {
         mHelper = new SiteHelper(context);
         mDatabase = mHelper.getWritableDatabase();
     }
 
-    public void insertAllSites(ArrayList<VisitData> listMovies, boolean clearPrevious) {
+    public void insertAllSites(int table, ArrayList<VisitData> listMovies, boolean clearPrevious) {
         if (clearPrevious) {
-            deleteMovies();
+            deleteMovies(table);
         }
-        String sql = "INSERT INTO " + SiteHelper.ALL_SITES + " VALUES (?,?,?,?,?,?,?,?,?,?);";
+        String sql = "INSERT INTO " + (table == ALL ? SiteHelper.TABLE_ALL : table == LIKE ? SiteHelper.TABLE_LIKE : table == OFFER ? SiteHelper.TABLE_OFFER : SiteHelper.TABLE_VISIT) + " VALUES (?,?,?,?,?,?,?,?,?,?,?);";
         SQLiteStatement statement = mDatabase.compileStatement(sql);
         mDatabase.beginTransaction();
         for (int i = 0; i < listMovies.size(); i++) {
@@ -42,6 +46,7 @@ public class MDatabase {
             statement.bindString(8, visitData.getOffer_desc());
             statement.bindString(9, visitData.getSitedesc());
             statement.bindString(10, visitData.isLike() ? "1" : "0");
+            statement.bindString(11, visitData.isDelete() ? "1" : "0");
             statement.execute();
         }
         //set the transaction as successful and end the transaction
@@ -51,12 +56,12 @@ public class MDatabase {
     }
 
 
-    public void deleteMovies() {
-        mDatabase.delete(SiteHelper.ALL_SITES, null, null);
+    public void deleteMovies(int table) {
+        mDatabase.delete(table == ALL ? SiteHelper.TABLE_ALL : table == LIKE ? SiteHelper.TABLE_LIKE : table == OFFER ? SiteHelper.TABLE_OFFER : SiteHelper.TABLE_VISIT, null, null);
     }
 
 
-    public ArrayList<VisitData> getAllSites() {
+    public ArrayList<VisitData> getAllSites(int table) {
         ArrayList<VisitData> listMovies = new ArrayList<>();
 
         String[] columns = {SiteHelper.COLUMN_UID,
@@ -69,9 +74,10 @@ public class MDatabase {
                 SiteHelper.COLUMN_OFFER_DESC,
                 SiteHelper.COLUMN_LIKE,
                 SiteHelper.COLUMN_SITE_DESC,
+                SiteHelper.COLUMN_DELETE
 
         };
-        Cursor cursor = mDatabase.query(SiteHelper.ALL_SITES, columns, null, null, null, null, null);
+        Cursor cursor = mDatabase.query((table == ALL ? SiteHelper.TABLE_ALL : table == LIKE ? SiteHelper.TABLE_LIKE : table == OFFER ? SiteHelper.TABLE_OFFER : SiteHelper.TABLE_VISIT), columns, null, null, null, null, null);
         if (cursor != null && cursor.moveToFirst()) {
             // L.m("loading entries " + cursor.getCount() + new Date(System.currentTimeMillis()));
             do {
@@ -86,6 +92,7 @@ public class MDatabase {
                 visitData.setOffer_desc(cursor.getString(cursor.getColumnIndex(SiteHelper.COLUMN_OFFER_DESC)));
                 visitData.setSitedesc(cursor.getString(cursor.getColumnIndex(SiteHelper.COLUMN_SITE_DESC)));
                 visitData.setLike(cursor.getString(cursor.getColumnIndex(SiteHelper.COLUMN_LIKE)).equals("1") ? true : false);
+                visitData.setDelete(cursor.getString(cursor.getColumnIndex(SiteHelper.COLUMN_DELETE)).equals("1") ? true : false);
 
                 listMovies.add(visitData);
             }
@@ -96,7 +103,10 @@ public class MDatabase {
 
 
     private static class SiteHelper extends SQLiteOpenHelper {
-        public static final String ALL_SITES = "all_sites";
+        public static final String TABLE_ALL = "all_sites";
+        public static final String TABLE_LIKE = "like_sites";
+        public static final String TABLE_OFFER = "offer_sites";
+        public static final String TABLE_VISIT = "visit_sites";
         public static final String COLUMN_UID = "_id";
         public static final String COLUMN_SITENAME = "sitename";
         public static final String COLUMN_SITE_ID = "siteid";
@@ -106,9 +116,10 @@ public class MDatabase {
         public static final String COLUMN_BID = "bid";
         public static final String COLUMN_OFFER_DESC = "offerdesc";
         public static final String COLUMN_LIKE = "like";
+        public static final String COLUMN_DELETE = "delete";
         public static final String COLUMN_SITE_DESC = "sitedesc";
 
-        private static final String CREATE_TABLE_ALL_SITES = "CREATE TABLE " + ALL_SITES + " (" +
+        private static final String CREATE_TABLE_ALL_SITES = "CREATE TABLE " + TABLE_ALL + " (" +
                 COLUMN_UID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_SITENAME + " TEXT," +
                 COLUMN_SITE_ID + " TEXT," +
@@ -118,7 +129,47 @@ public class MDatabase {
                 COLUMN_BID + " TEXT," +
                 COLUMN_OFFER_DESC + " TEXT," +
                 COLUMN_SITE_DESC + " TEXT," +
-                COLUMN_LIKE + " TEXT" +
+                COLUMN_LIKE + " TEXT," +
+                COLUMN_DELETE + " TEXT" +
+                ");";
+        private static final String CREATE_TABLE_LIKE_SITES = "CREATE TABLE " + TABLE_LIKE + " (" +
+                COLUMN_UID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_SITENAME + " TEXT," +
+                COLUMN_SITE_ID + " TEXT," +
+                COLUMN_SITEICON + " TEXT," +
+                COLUMN_NUMBER + " TEXT," +
+                COLUMN_OFFER + " TEXT," +
+                COLUMN_BID + " TEXT," +
+                COLUMN_OFFER_DESC + " TEXT," +
+                COLUMN_SITE_DESC + " TEXT," +
+                COLUMN_LIKE + " TEXT," +
+                COLUMN_DELETE + " TEXT" +
+                ");";
+        private static final String CREATE_TABLE_OFFER_SITES = "CREATE TABLE " + TABLE_OFFER + " (" +
+                COLUMN_UID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_SITENAME + " TEXT," +
+                COLUMN_SITE_ID + " TEXT," +
+                COLUMN_SITEICON + " TEXT," +
+                COLUMN_NUMBER + " TEXT," +
+                COLUMN_OFFER + " TEXT," +
+                COLUMN_BID + " TEXT," +
+                COLUMN_OFFER_DESC + " TEXT," +
+                COLUMN_SITE_DESC + " TEXT," +
+                COLUMN_LIKE + " TEXT," +
+                COLUMN_DELETE + " TEXT" +
+                ");";
+        private static final String CREATE_TABLE_VISIT_SITES = "CREATE TABLE " + TABLE_VISIT + " (" +
+                COLUMN_UID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_SITENAME + " TEXT," +
+                COLUMN_SITE_ID + " TEXT," +
+                COLUMN_SITEICON + " TEXT," +
+                COLUMN_NUMBER + " TEXT," +
+                COLUMN_OFFER + " TEXT," +
+                COLUMN_BID + " TEXT," +
+                COLUMN_OFFER_DESC + " TEXT," +
+                COLUMN_SITE_DESC + " TEXT," +
+                COLUMN_LIKE + " TEXT," +
+                COLUMN_DELETE + " TEXT" +
                 ");";
 
         private static final String DB_NAME = "allsites_db";
@@ -135,6 +186,9 @@ public class MDatabase {
         public void onCreate(SQLiteDatabase db) {
             try {
                 db.execSQL(CREATE_TABLE_ALL_SITES);
+                db.execSQL(CREATE_TABLE_LIKE_SITES);
+                db.execSQL(CREATE_TABLE_OFFER_SITES);
+                db.execSQL(CREATE_TABLE_VISIT_SITES);
                 // L.m("create table box office executed");
             } catch (SQLiteException exception) {
                 // L.t(mContext, exception + "");
@@ -146,7 +200,10 @@ public class MDatabase {
             try {
                 // L.m("upgrade table box office executed");
 
-                db.execSQL(" DROP TABLE " + ALL_SITES + " IF EXISTS;");
+                db.execSQL(" DROP TABLE " + TABLE_ALL + " IF EXISTS;");
+                db.execSQL(" DROP TABLE " + TABLE_LIKE + " IF EXISTS;");
+                db.execSQL(" DROP TABLE " + TABLE_VISIT + " IF EXISTS;");
+                db.execSQL(" DROP TABLE " + TABLE_OFFER + " IF EXISTS;");
                 onCreate(db);
             } catch (SQLiteException exception) {
                 //  L.t(mContext, exception + "");
